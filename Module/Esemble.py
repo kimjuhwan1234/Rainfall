6a5c5e1df11d73c0793b746ec6bfabaa8f57989b
Kim Juhwan <asas4539@hanyang.ac.kr>
2024-06-20 16:37:35 +0900

    .

--- Module/Esemble.py
+++ Module/Esemble.py
@@ -1,74 +1,71 @@
 import joblib
 from sklearn.metrics import *
-from xgboost import XGBClassifier
-from lightgbm import LGBMClassifier
-from catboost import CatBoostClassifier
-from sklearn.tree import DecisionTreeClassifier
+from xgboost import XGBRegressor
+from lightgbm import LGBMRegressor
+from catboost import CatBoostRegressor
+from sklearn.tree import DecisionTreeRegressor
 
 
 class Esemble:
-    def __init__(self, method, X_train, X_val, y_train, y_val, num_rounds, sampling_name):
+    def __init__(self, method, X_train, X_val, y_train, y_val, num_rounds, name):
         self.method = method
         self.X_train = X_train
         self.X_test = X_val
         self.y_train = y_train
         self.y_test = y_val
         self.num_rounds = num_rounds
-        self.name = sampling_name
+        self.name = name
 
-    def save_dict_to_txt(self, file_path, dictionary):
+    def save_dict_to_txt(self, file_path, dict):
         with open(file_path, 'w') as f:
-            for key, value in dictionary.items():
+            for key, value in dict.items():
                 f.write(f'{key}: {value}\n')
 
     def DecisionTree(self, params):
-        bst = DecisionTreeClassifier(**params)
+        bst = DecisionTreeRegressor(**params)
         bst.fit(self.X_train, self.y_train)
-        y_pred = bst.predict_proba(self.X_test)
-        predictions = y_pred.argmax(axis=1)
-        accuracy = f1_score(self.y_test, predictions, average='micro')
+        y_pred = bst.predict(self.X_test)
 
-        # joblib.dump(bst, f'Files/dt_{self.name}_model.pkl')
-        print("DT F1-Score:", accuracy)
-        return accuracy
+        mape = mean_absolute_percentage_error(self.y_test, y_pred)
+        R_squre = r2_score(self.y_test, y_pred)
+        print("DT R-square:", R_squre)
+        return mape
 
     def lightGBM(self, params):
-        bst = LGBMClassifier(**params, n_estimators=self.num_rounds, verbose_eval=100)
+        bst = LGBMRegressor(**params, n_estimators=self.num_rounds, verbose_eval=100)
         bst.fit(self.X_train, self.y_train, eval_set=[(self.X_test, self.y_test)])
-        y_pred = bst.predict_proba(self.X_test)
-        predictions = y_pred.argmax(axis=1)
-        accuracy = f1_score(self.y_test, predictions, average='micro')
+        y_pred = bst.predict(self.X_test)
 
-        # joblib.dump(bst, f'Files/lgb_{self.name}_model.pkl')
-        print("lightGBM F1-Score:", accuracy)
-        return accuracy
+        mape = mean_absolute_percentage_error(self.y_test, y_pred)
+        R_squre = r2_score(self.y_test, y_pred)
+        print("LGBM R-square:", R_squre)
+        return mape
 
     def XGBoost(self, params):
-        bst = XGBClassifier(**params, n_estimators=self.num_rounds)
+        bst = XGBRegressor(**params, n_estimators=self.num_rounds)
         bst.fit(self.X_train, self.y_train, eval_set=[(self.X_test, self.y_test)], verbose=100)
-        y_pred = bst.predict_proba(self.X_test)
-        predictions = y_pred.argmax(axis=1)
-        accuracy = f1_score(self.y_test, predictions, average='micro')
+        y_pred = bst.predict(self.X_test)
 
-        # joblib.dump(bst, f'Files/xgb_{self.name}_model.pkl')
-        print("XGBoost F1-Score:", accuracy)
-        return accuracy
+        mape = mean_absolute_percentage_error(self.y_test, y_pred)
+        R_squre = r2_score(self.y_test, y_pred)
+        print("XGB R-square:", R_squre)
+        return mape
 
     def CatBoost(self, params):
-        bst = CatBoostClassifier(**params, iterations=self.num_rounds)
+        bst = CatBoostRegressor(**params, iterations=self.num_rounds)
         bst.fit(self.X_train, self.y_train, eval_set=[(self.X_test, self.y_test)], verbose=100)
-        y_pred = bst.predict_proba(self.X_test)
-        predictions = y_pred.argmax(axis=1)
-        accuracy = f1_score(self.y_test, predictions, average='micro')
+        y_pred = bst.predict(self.X_test)
 
-        # joblib.dump(bst, f'Files/cat_{self.name}_model.pkl')
-        print("CatBoost F1-Score:", accuracy)
-        return accuracy
+        mape = mean_absolute_percentage_error(self.y_test, y_pred)
+        R_squre = r2_score(self.y_test, y_pred)
+        print("CAT R-square:", R_squre)
+        return mape
 
     def objective(self, trial):
         if self.method == 0:
             params = {
-                'criterion': 'entropy',
+                'criterion': 'friedman_mse',
+
                 'max_features': trial.suggest_float('max_features', 0.1, 0.9),
                 'max_depth': trial.suggest_int('max_depth', 5, 20),
             }
@@ -76,46 +73,43 @@ class Esemble:
 
         if self.method == 1:
             params = {
-                'device': 'cpu',
-                'num_class': 7,
+                'device': 'gpu',
+                'objective': 'regression',
+                'metric': 'mape',
+                'boosting_type': 'gbdt',
+                'learning_rate': 0.01,
                 'early_stopping_rounds': 10,
-                'objective': 'multiclass',
-                'metric': 'multi_logloss',
-                'boosting_type': 'gbrt',
-                'tree_learner': 'voting',
 
-                'learning_rate': trial.suggest_float('learning_rate', 0.01, 0.5),
                 'max_depth': trial.suggest_int('max_depth', 5, 20),
-                # 'num_leaves': trial.suggest_int('num_leaves', 50, 300),
+                'lambda_l2': trial.suggest_float('reg_lambda', 1e-5, 10.0),
+
             }
             accuracy = self.lightGBM(params)
 
         if self.method == 2:
             params = {
-                'device': 'cuda',
-                'num_class': 7,
-                'early_stopping_rounds': 10,
-                'objective': 'multi:softprob',
-                'eval_metric': 'mlogloss',
+                'tree_method': 'gpu_hist',
+                'objective': 'reg:squarederror',
+                'eval_metric': 'mape',
                 'booster': 'gbtree',
+                'eta': 0.01,
+                'early_stopping_rounds': 10,
 
-                'eta': trial.suggest_float('eta', 0.01, 0.5),
                 'max_depth': trial.suggest_int('max_depth', 5, 20),
-                # 'min_child_weight': trial.suggest_int('min_child_weight', 1, 10),
+                'reg_lambda': trial.suggest_float('reg_lambda', 1e-5, 10.0),
             }
             accuracy = self.XGBoost(params)
 
         if self.method == 3:
             params = {
                 'task_type': 'GPU',
-                'classes_count': 7,
+                'objective': 'RMSE',
+                'eval_metric': 'MAPE',
+                'learning_rate': 0.01,
                 'early_stopping_rounds': 10,
-                'loss_function': 'MultiClass',
-                'eval_metric': 'MultiClass',
-                'grow_policy': 'Lossguide',
 
-                'learning_rate': trial.suggest_float('learning_rate', 0.01, 0.5),
-                'depth': trial.suggest_int('depth', 5, 16),
+                'depth': trial.suggest_int('max_depth', 5, 20),
+                'l2_leaf_reg': trial.suggest_float('reg_lambda', 1e-5, 10.0),
             }
             accuracy = self.CatBoost(params)
 
@@ -124,65 +118,63 @@ class Esemble:
     def save_best_model(self, best_params):
         if self.method == 0:
             best_params.update({
-                'criterion': 'entropy',
+                'criterion': 'friedman_mse',
             })
 
-            bst = DecisionTreeClassifier(**best_params)
+            bst = DecisionTreeRegressor(**best_params)
             bst.fit(self.X_train, self.y_train)
-            joblib.dump(bst, f'Files/dt_{self.name}_model.pkl')
-            self.save_dict_to_txt(f'Files/dt_{self.name}_params.txt', best_params)
-            print(f'{f1_score(self.y_test, bst.predict(self.X_test), average="micro"):.4f}')
+            joblib.dump(bst, f'File/DT/dt_{self.name}_model.pkl')
+            self.save_dict_to_txt(f'File/DT/dt_{self.name}_params.txt', best_params)
+            print(f'{mean_absolute_percentage_error(self.y_test, bst.predict(self.X_test)):.4f}')
             print("Model saved!")
 
         if self.method == 1:
             best_params.update({
-                'device': 'cpu',
-                'num_class': 7,
+                'device': 'gpu',
+                'objective': 'regression',
+                'metric': 'mape',
+                'boosting_type': 'gbdt',
+                'learning_rate': 0.01,
                 'early_stopping_rounds': 10,
-                'objective': 'multiclass',
-                'metric': 'multi_logloss',
-                'boosting_type': 'gbrt',
-                'tree_learner': 'voting',
+
             })
 
-            bst = LGBMClassifier(**best_params)
+            bst = LGBMRegressor(**best_params)
             bst.fit(self.X_train, self.y_train, eval_set=[(self.X_test, self.y_test)])
-            joblib.dump(bst, f'Files/lgb_{self.name}_model.pkl')
-            self.save_dict_to_txt(f'Files/lgb_{self.name}_params.txt', best_params)
-            print(f'{f1_score(self.y_test, bst.predict(self.X_test), average="micro"):.4f}')
+            joblib.dump(bst, f'File/LGBM/lgb_{self.name}_model.pkl')
+            self.save_dict_to_txt(f'File/LGBM/lgb_{self.name}_params.txt', best_params)
+            print(f'{mean_absolute_percentage_error(self.y_test, bst.predict(self.X_test)):.4f}')
             print("Model saved!")
 
         if self.method == 2:
             best_params.update({
-                'device': 'cuda',
-                'num_class': 7,
-                'early_stopping_rounds': 10,
-                'objective': 'multi:softprob',
-                'eval_metric': 'mlogloss',
+                'tree_method': 'gpu_hist',
+                'objective': 'reg:squarederror',
+                'eval_metric': 'mape',
                 'booster': 'gbtree',
+                'eta': 0.01,
+                'early_stopping_rounds': 10,
             })
 
-            bst = XGBClassifier(**best_params)
+            bst = XGBRegressor(**best_params)
             bst.fit(self.X_train, self.y_train, eval_set=[(self.X_test, self.y_test)], verbose=100)
-            joblib.dump(bst, f'Files/xgb_{self.name}_model.pkl')
-            self.save_dict_to_txt(f'Files/xgb_{self.name}_params.txt', best_params)
-            print(f'{f1_score(self.y_test, bst.predict(self.X_test), average="micro"):.4f}')
+            joblib.dump(bst, f'File/XGB/xgb_{self.name}_model.pkl')
+            self.save_dict_to_txt(f'File/XGB/xgb_{self.name}_params.txt', best_params)
+            print(f'{mean_absolute_percentage_error(self.y_test, bst.predict(self.X_test)):.4f}')
             print("Model saved!")
 
         if self.method == 3:
             best_params.update({
                 'task_type': 'GPU',
-                'classes_count': 7,
+                'objective': 'RMSE',
+                'eval_metric': 'MAPE',
+                'learning_rate': 0.01,
                 'early_stopping_rounds': 10,
-                'loss_function': 'MultiClass',
-                'eval_metric': 'MultiClass',
-                'grow_policy': 'Lossguide',
-                'bootstrap_type': 'Bayesian',
             })
 
-            bst = CatBoostClassifier(**best_params)
+            bst = CatBoostRegressor(**best_params)
             bst.fit(self.X_train, self.y_train, eval_set=[(self.X_test, self.y_test)], verbose=100)
-            joblib.dump(bst, f'Files/cat_{self.name}_model.pkl')
-            self.save_dict_to_txt(f'Files/cat_{self.name}_params.txt', best_params)
-            print(f'{f1_score(self.y_test, bst.predict(self.X_test), average="micro"):.4f}')
+            joblib.dump(bst, f'File/CAT/cat_{self.name}_model.pkl')
+            self.save_dict_to_txt(f'File/CAT/cat_{self.name}_params.txt', best_params)
+            print(f'{mean_absolute_percentage_error(self.y_test, bst.predict(self.X_test)):.4f}')
             print("Model saved!")
--- main.py
+++ main.py
@@ -1,22 +1,33 @@
+import os
 import optuna
 import warnings
 import pandas as pd
 from Module.Esemble import *
 
+
+
 if __name__ == "__main__":
     warnings.filterwarnings("ignore")
-    X_train = pd.read_csv('Database/X_train.csv', index_col=0)
-    y_train = pd.read_csv('Database/y_train.csv', index_col=0)
-    X_val = pd.read_csv('Database/X_val.csv', index_col=0)
-    y_val = pd.read_csv('Database/y_val.csv', index_col=0)
+    STN_list = ['STN001', 'STN002', 'STN003', 'STN004', 'STN005', 'STN006', 'STN007', 'STN008', 'STN009', 'STN010',
+                'STN011', 'STN012', 'STN013', 'STN014', 'STN015', 'STN016', 'STN017', 'STN018', 'STN019', 'STN020']
+    directory = 'Database/train/'
+    files = sorted(filename for filename in os.listdir(directory) if filename.endswith('.csv'))
 
-    for i in range(4):
-        # {DT=0, lightGBM=1, XGBoost=2, CatBoost=3}
-        E = Esemble(i, X_train, X_val, y_train, y_val, 1000, name)
+    X_val = pd.read_csv('Database/val/X_val_norm.csv', index_col=0)
+    y_val = pd.read_csv('Database/val/y_val.csv', index_col=0)
 
-        study = optuna.create_study(direction='maximize')
-        study.optimize(E.objective, n_trials=100)
+    for i, STN in enumerate(STN_list):
+        file_list = [sentence for sentence in files if STN in sentence]
+        X_train = pd.read_csv(os.path.join(directory, file_list[0]), index_col=0)
+        y_train = pd.read_csv(os.path.join(directory, file_list[1]), index_col=0)
 
-        print('Number of finished trials:', len(study.trials))
-        print('Best trial:', study.best_trial.params)
-        E.save_best_model(study.best_trial.params)
+        for i in range(3, 4):
+            # {DT=0, lightGBM=1, XGBoost=2, CatBoost=3}
+            E = Esemble(i, X_train, X_val, y_train, y_val, 1000, STN)
+
+            study = optuna.create_study(direction='maximize')
+            study.optimize(E.objective, n_trials=100)
+
+            print('Number of finished trials:', len(study.trials))
+            print('Best trial:', study.best_trial.params)
+            E.save_best_model(study.best_trial.params)
