f6a1412eeb94d153f14ca03b9dd9d817244d915b
Kim Juhwan <asas4539@hanyang.ac.kr>
2024-06-23 17:52:58 +0900

    .

--- Module/dataset.py
+++ Module/dataset.py
@@ -16,18 +16,3 @@ class CustomDataset(Dataset):
         y_train_tensor = torch.tensor(self.y.iloc[idx].values)
 
         return X_train_tensor, y_train_tensor
-
-
-class TestDataset(Dataset):
-    def __init__(self, X, y):
-        self.X = X
-        self.y = y
-
-    def __len__(self):
-        return len(self.X)
-
-    def __getitem__(self, idx):
-        X_train_tensor = torch.tensor(self.X.iloc[idx, :].values)
-        y_train_tensor = torch.tensor(self.y.iloc[idx].values)
-
-        return X_train_tensor, y_train_tensor
