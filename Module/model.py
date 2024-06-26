c045286aceaab11c38f4df1077c24ce8b081ad0c
Kim Juhwan <asas4539@hanyang.ac.kr>
2024-06-26 18:30:01 +0900

    .

--- Module/model.py
+++ Module/model.py
@@ -96,6 +96,7 @@ class VAE(nn.Module):
         x_recon = torch.cat((x_recon_input, x_recon_target), dim=1)
         x_gt = x[:, -1]
         target_loss = nn.functional.cross_entropy(x_recon_input.float(), x_gt.long())
-        total_loss = (loss1 + loss2) * torch.log_(target_loss)
+        loged_target_loss = torch.log_(target_loss)
+        total_loss = (loss1 + loss2) * loged_target_loss
 
         return x_recon, total_loss, target_loss
