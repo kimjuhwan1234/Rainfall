1a00391180d04f80fefde17e0f995bc6bf1b45d7
Kim Juhwan <asas4539@hanyang.ac.kr>
2024-06-27 17:58:45 +0900

    .

--- main.py
+++ main.py
@@ -15,12 +15,13 @@ if __name__ == "__main__":
 
     X_val = pd.read_csv('Database/val/X_val_norm.csv', index_col=0)
     y_val = pd.read_csv('Database/val/y_val.csv', index_col=0)
-    for i in range(0, 3):
-        for i, code in enumerate(GMM_list):
-            file_list = [sentence for sentence in files if code in sentence]
-            X_train = pd.read_csv(os.path.join(directory, file_list[0]), index_col=0)
-            y_train = pd.read_csv(os.path.join(directory, file_list[1]), index_col=0)
 
+    for i, code in enumerate(GMM_list):
+        file_list = [sentence for sentence in files if code in sentence]
+        X_train = pd.read_csv(os.path.join(directory, file_list[0]), index_col=0)
+        y_train = pd.read_csv(os.path.join(directory, file_list[1]), index_col=0)
+
+        for i in range(0, 1):
             # {DT=0, CatBoost=1, lightGBM=2, XGBoost=3}
             E = Esemble(i, X_train, X_val, y_train, y_val, 1000, code)
 
