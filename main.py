e3a0b6896e49f91835233621966a532c860eb85e
Kim Juhwan <asas4539@hanyang.ac.kr>
2024-06-27 16:58:00 +0900

    .

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     STN_list = ['STN001', 'STN002', 'STN003', 'STN004', 'STN005', 'STN006', 'STN007', 'STN008', 'STN009', 'STN010',
                 'STN011', 'STN012', 'STN013', 'STN014', 'STN015', 'STN016', 'STN017', 'STN018', 'STN019', 'STN020']
 
-    GMM_list = [f'GMM{i}' for i in range(15)]
+    GMM_list = [f'Random{i}' for i in range(20)]
     directory = 'Database/train/'
     files = sorted(filename for filename in os.listdir(directory) if filename.endswith('.csv'))
 
@@ -26,7 +26,7 @@ if __name__ == "__main__":
             E = Esemble(i, X_train, X_val, y_train, y_val, 1000, code)
 
             study = optuna.create_study(direction='maximize', sampler=optuna.samplers.RandomSampler())
-            study.optimize(E.objective, n_trials=100)
+            study.optimize(E.objective, n_trials=20)
 
             print('Number of finished trials:', len(study.trials))
             print('Best trial:', study.best_trial.params)
